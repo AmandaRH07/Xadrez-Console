51fa23beec07fce06266ae32bbec052e105e723c
AmandaRH07 <amandarafahass13@gmail.com>
2020-07-26 21:23:59 -0300

    Comecando a colocar pecas no tabuleiro

--- Xadrez-Console/Tela.cs
+++ Xadrez-Console/Tela.cs
@@ -9,7 +9,7 @@ namespace Xadrez_Console {
             for (int i = 0; i < tab.linhas; i++) {
                 for (int j = 0; j < tab.linhas; j++) {
                     if (tab.peca(i, j) == null) {
-                        Console.Write("* ");
+                        Console.Write("- ");
                     }
                     Console.Write(tab.peca(i, j) + " ");
                 }
